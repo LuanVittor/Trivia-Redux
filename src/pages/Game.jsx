a6b9a78fbe7a06ce9f27c4798efe923ba08f212c
Luan Vitor <luan_vittor@hotmail.com>
2021-11-24 20:59:18 -0300

    Five and Six done

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import RenderQuestions from '../components/RenderQuestions';
 
 export default class Game extends Component {
   render() {
     return (
       <div>
         <Header />
+        <RenderQuestions />
       </div>
     );
   }
