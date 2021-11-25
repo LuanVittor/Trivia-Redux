ff580e46ecf8327a3bcf88599eaff45146c2aa4a
Luan Vitor <luan_vittor@hotmail.com>
2021-11-25 15:58:19 -0300

    eight done

--- src/components/RenderQuestions.jsx
+++ src/components/RenderQuestions.jsx
@@ -6,17 +6,20 @@ export default class RenderQuestions extends Component {
     super();
 
     this.getQuestions = this.getQuestions.bind(this);
+    this.timeout = this.timeout.bind(this);
 
     this.state = {
       questions: [],
       index: 0,
       renderQ: true,
       show: false,
+      timer: 30,
     };
   }
 
   componentDidMount() {
     this.getQuestions();
+    this.timeout();
   }
 
   getQuestions() {
@@ -26,23 +29,51 @@ export default class RenderQuestions extends Component {
       .then((r) => this.setState({ questions: r, renderQ: false }));
   }
 
+  stopInverval(param) {
+    clearInterval(param);
+  }
+
+  timeout() {
+    const MIL = 1000;
+    const timer2 = setInterval(() => {
+      const { timer } = this.state;
+      this.setState({ timer: timer - 1 });
+      if (timer === 1) {
+        this.stopInverval(timer2);
+        this.showAnswers();
+        return this.setState({ timer: 0 });
+      }
+    }, MIL);
+  }
+
   showAnswers() {
     this.setState({ show: true });
+    this.disableButton();
+  }
+
+  disableButton() {
+    const { show } = this.state;
+    if (show) {
+      return true;
+    }
+    return false;
   }
 
   render() {
-    const { questions, index, renderQ, show } = this.state;
+    const { questions, index, renderQ, show, timer } = this.state;
     if (renderQ) {
       return (
-        <p>Loading</p>
+        <p>Loading...</p>
       );
     }
     return (
       <div>
+        {timer}
         <p data-testid="question-category">{ questions.results[index].category }</p>
         <p data-testid="question-text">{ questions.results[index].question }</p>
         <button
           type="button"
+          disabled={ this.disableButton() }
           data-testid="correct-answer"
           onClick={ () => this.showAnswers() }
           className={ (show) ? 'green-border' : null }
@@ -53,6 +84,7 @@ export default class RenderQuestions extends Component {
           .map((elem, i) => (
             <button
               onClick={ () => this.showAnswers() }
+              disabled={ this.disableButton() }
               className={ (show) ? 'red-border' : null }
               key={ i }
               type="button"
